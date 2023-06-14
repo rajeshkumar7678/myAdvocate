fe69edda709e592b6b55a04f66a30471932b27bb
Adesh856 <adeshtayde6@gmail.com>
2023-06-15 02:08:56 +0530

    Day1-firstUpdate

---
+++ Chats_Folder/Backend/routes/users.routes.js
@@ -0,0 +1,31 @@
+//------Dependencies------
+const express = require("express")
+const usersRouter =express.Router()
+const userModel = require("../models/Users.model")
+/////------Extra Middleware built-in function
+
+
+
+
+
+
+
+////----------Routes----------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=usersRouter
