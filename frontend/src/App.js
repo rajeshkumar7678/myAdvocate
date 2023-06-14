f307db61344b01548fdb791ba43faf272787fbe0
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-06-14 20:58:15 +0530

    register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
       
+      <Register />
     </div>
   );
 }
