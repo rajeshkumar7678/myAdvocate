221346b78397c8e850c7a56c678e618ebe066946
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-06-15 11:32:16 +0530

    day-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
       <Register />
     </div>
   );
