529853d51646e03b4b6c93c972acca042edc27ca
Adesh856 <adeshtayde6@gmail.com>
2023-06-16 04:10:55 +0530

    Day2_Adesh

--- Chats_Folder/Backend/index.js
+++ Chats_Folder/Backend/index.js
@@ -1,28 +1,46 @@
 ////-------Depenencies----------
 const express = require("express")
 const app = express()
-const connection = require("./db")
 const messagesRouter = require("./routes/message.routes")
 const usersRouter = require("./routes/users.routes")
-require("dotenv").config
+const socket = require("socket.io")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
 
+// Connected with mongoDB
+mongoose.connect(process.env.mongoURL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ }).then(()=>{
+    console.log("MongoDB is connected with Server")
+ }).catch((err)=>{
+    console.log("MongoDB is not connected with Server")
+ })
+
+//Server running 
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server is listening on : ${process.env.PORT}`)
+})
 //------Extra Middleware built-in function
 app.use(express.json())
-
+app.use(cors())
 //-------------Routes----------------
 app.get("/",(req,res)=>{
     res.send("Chat APP")
 })
 app.use("/users",usersRouter)
 app.use("/message",messagesRouter)
+global.onlineUsers = new Map()
+ const io = socket(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        credentials:true
+    }
+ })
 
 
-////---------Listening----------------
-app.listen(process.env.PORT,async()=>{
-        try {
-            
-        } catch (error) {
-            console.log("MongoDB is not connected with Server")
-        }
-        console.log(`Server is listening on : ${process.env.PORT}`)
+io.on("connection",(socket)=>{
+    console.log("Connected Successfully")
 })
+ 
--- Chats_Folder/Backend/models/message.model.js
+++ Chats_Folder/Backend/models/message.model.js
@@ -8,9 +8,10 @@ const MessageSchema = mongoose.Schema(
     users: Array,
     sender: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "userModel",
       required: true,
     },
+    
   },
   {
     timestamps: true,
