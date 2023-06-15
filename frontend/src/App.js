503b00e4037eb62c492f3f5477bc5f97729e298c
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-06-15 23:12:58 +0530

    login,signup,userModel,userRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       
-      <Register />
+
+      <Router>
+       <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      </Routes>
+     </Router>
+     
     </div>
   );
 }
