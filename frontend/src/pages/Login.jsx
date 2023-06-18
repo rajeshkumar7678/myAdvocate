be56c5aaaa284ed4cac762918d687202cea878c4
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-06-18 09:11:27 +0530

    day-5

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,41 +1,163 @@
 import React from "react";
 import "../Components/Login.css"
 
-const Login = ()=>{
-
-    return  <div className="main-div">
-
-   <div className="image-div"></div>
-
-    <div className="form-div">
-    <form  action="">
+// import  { useContext,  useState } from "react";
+import { Link} from "react-router-dom";
+// import HOST from "../Utils/baseURL";
 
 
-                    <fieldset>
-                     <legend>Enter Your Email</legend>
-                     <input type="email" name="" id="email"/>
-                    </fieldset>
-                   <fieldset>
-                     <legend>Enter Your password</legend>
-                     <input type="password" id="password"/>
-                  </fieldset>
-                      <fieldset>
-                         <legend>click on Sign in</legend>
-                         <input  id="submit" type="submit" value="Sign In"/>
-                      </fieldset>
+const Login = () => {
+//   const { UserDetails, setUserDetails } = useContext(UserContext);
+//   const { Auth, setAuth } = useContext(AuthContext);
 
-    
-    </form>
+
+//   const signIN = async (data) => {
+//     const response = await fetch(`${HOST}/user/login`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ email: data.email, password: data.password }),
+//     });
+
+//     // const Data = await response.json();
+//     // if (Data.status === "success") {
+//     //   localStorage.setItem("token", Data.token);
+//     //   setUserDetails(Data.userData);
+//     //   setAuth(true);
+//     //   setTimeout(() => {
+//     //     setAuth(true);
+//     //     console.log("Login Success");
+//     //     navigate("/userdesh");
+//     //   }, 1000);
+//     // } else {
+//     //   console.log("Invalid Credentials", "Enter valid account details.");
+//     // }
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     if (value === "User Email") {
+//       let data = {
+//         email,
+//         password,
+//       };
+//       signIN(data);
+//     } else if (value === "Lawyer ID") {
+//       console.log("Hello from lawyer");
+//     } else {
+//       if (email === "admin@gmail.com" && password === "admin") {
+//         // setAuth(true);
+//         // navigate("/admin");
+//       }
+//     }
+//   };
+  
+  return (
+    <div className="form-container">
+      {/* {contextHolder} */}
+      <p className="logintitle">Login</p>
+      <div className="labeldiv">
+        <label
+        //   onClick={() => handleClick("User Email")}
+        //   style={{
+        //     backgroundColor: value !== "User Email" ? "#fafafa" : "#ffd41f",
+        //     border: "none",
+        //   }}
+        >
+          User
+        </label>
+        <label
+        //   onClick={() => handleClick("Lawyer ID")}
+        //   style={{
+        //     backgroundColor: value !== "Lawyer ID" ? "#fafafa" : "#ffd41f",
+        //     border: "none",
+        //   }}
+        >
+          Lawyer
+        </label>
+        <label
+        //   onClick={() => handleClick("Admin ID")}
+        //   style={{
+        //     backgroundColor: value !== "Admin ID" ? "#fafafa" : "#ffd41f",
+        //     border: "none",
+        //   }}
+        >
+          Admin
+        </label>
+      </div>
+      <div className="userpng">
+        <img src="" alt="" />
+      </div>
+      <div>
+        <form className="form" > 
+        
+          <div className="input-group">
+            <label htmlFor="username" className="fontweightfive">
+              {/* {value} */}
+            </label>
+            <input
+            //   onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              name="username"
+              id="username"
+              placeholder="User Name"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password" className="fontweightfive">
+              Password
+            </label>
+            <input
+            //   onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+
+            <div className="forgot">
+              <div rel="noopener noreferrer" className="yellohover">
+                {/* <ForgotModal /> */}
+              </div>
+            </div>
+          </div>
+          <button className="signInBtn" type="submit">
+            Sign in
+          </button>
+        </form>
+        <div className="social-message">
+          <div className="line"></div>
+          <p className="message">Login with social accounts</p>
+          <div className="line"></div>
+        </div>
+
+        <div className="SocialIcons">
+          <div>
+            <img
+              style={{ width: "30px" }}
+              src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+              alt="icon"
+            />
+            <label>Google Login</label>
+          </div>
+          
+        </div>
+        <p className="signup">
+          Don't have an account?
+          <Link to="/register" style={{ color: "teal" }}>
+            &nbsp; Sign up
+          </Link>
+        </p>
+      </div>
     </div>
-    
-    </div>;
+  );
+};
+
+export default Login;
 
 
+  
 
-
-     
-}
-
-
-
-export default Login
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,55 +1,166 @@
 import React from "react";
 import "../Components/Register.css"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import HOST from "../Utils/baseURL";
+import imageFileName from '../images/home1.jpg';
 
 
-const Register = ()=>{
-
-    return  <div className="main-div">
-
-      <div className="image-div">
-
-      </div>
-
-      <div className="form-div">
-      <h1>Register</h1>
+const Register = () => {
+  // const navigate = useNavigate();
 
   
-    <form  action="">
 
-    <fieldset>
-                      
-                      <legend>Enter Your Name</legend>
-                      <input type="text" id="name" />
-                   </fieldset>
-                    <fieldset>
-                     <legend>Enter Your Email</legend>
-                     <input type="email" name="" id="email"/>
-                    </fieldset>
-                   <fieldset>
-                     <legend>Enter Your password</legend>
-                     <input type="password" id="password"/>
-                  </fieldset>
-                      <fieldset>
-                         <legend>click on submit</legend>
-                         <input  id="submit" type="submit" value="Sign Up"/>
-                      </fieldset>
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [lawyer, setLawyer] = useState("");
+  let [user, setUser] = useState("");
+  let [password, setPassword] = useState("");
 
-                     
-                      <button id="log-n"> <a href="./login">Existing User? login </a></button>
-
+  const signUp = async (data) => {
+    const response = await fetch(`${HOST}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const json = await response.json();
+    console.log(json);
     
-    </form>
+  };
 
-    </div>
-    
-    </div>;
-
-
-
-
+  const handleSubmit = () => {
+    let data = {
+      email,
+      password,
+      name,
+      user,
+      lawyer,
+    };
+    signUp(data);
+  };
+  return (
+      
+    <div className="signUpdiv">
      
-}
+      {/* {contextHolder} */}
+      <div className="left-div">
+        <img  src={imageFileName} alt="acelogo" />
+      </div>
+      <div className="right-div">
+        <form
+          className="Signupform"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <h1 className="RegisterTitle">Register</h1>
+          <p style={{ color: "grey", fontSize: "13px" }}>
+             Signup to MyAdvocate
+          </p>
+          <fieldset>
+            <legend>Enter user name</legend>
+          <input
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            type="text"
+            className="SignupUserName"
+            placeholder="&nbsp;&nbsp;Username"
+            required
+          />
+          </fieldset>
+        
+        
+        <fieldset>
+          <legend>Enter Your Email</legend>
+          <input
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            type="email"
+            className="SignupEmail"
+            placeholder="&nbsp;&nbsp;Email"
+            required
+          />
+          </fieldset>
+
+          <fieldset>
+            <legend>Enter Password</legend>
+          <input
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            type="password"
+            className="SignupPass"
+            placeholder="&nbsp;&nbsp;Password"
+            required
+          />
+          </fieldset>
+
+
+<label htmlFor="user">User</label>
+        <input
+  onChange={(event) => {
+    setUser(event.target.value);
+  }}
+  type="radio"
+  name="userType"
+  value="user"
+  className="SignupUser"
+  required
+/>
+
+<label htmlFor="lawyer">Lawyer</label>
+
+<input
+  onChange={(event) => {
+    setLawyer(event.target.value);
+  }}
+  type="radio"
+  name="userType"
+  value="lawyer"
+  className="SignupLawyer"
+  required
+/>
 
 
 
-export default Register
+          <span className="alreadyacc">
+            Already have an account ?
+            <Link className="SignInSmol" to="/login">
+              &nbsp; Sign in
+            </Link>
+          </span>
+          <input
+            type="submit"
+            value="Continue"
+            style={{ cursor: "pointer", margin: "0" }}
+            className="ContinueRegis"
+          />
+
+          <div className="social-message">
+            <div className="line">-</div>
+            <p className="message">Login with social accounts</p>
+            <div className="line">-</div>
+          </div>
+          <div className="SocialIcons">
+            <div>
+              <img
+                style={{ width: "25px" }}
+                src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+                alt="icon"
+              />
+              <label>Continue With Google</label>
+            </div>
+            
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
