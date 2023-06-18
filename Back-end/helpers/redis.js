f312733dd6c3161873cdfb41b6db5f10463b01df
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-06-18 09:10:56 +0530

    day-5

--- Back-end/helpers/redis.js
+++ Back-end/helpers/redis.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const Redis = require("ioredis");
+// const express = require("express");
+// const Redis = require("ioredis");
 
-const app = express();
+// const app = express();
 
-require("dotenv").config()
-const configuration={
-    host:process.env.redisurl,
-    port:19701,
-    username:"default",
-    password: process.env.redispassword
+// require("dotenv").config()
+// const configuration={
+//     host:process.env.redisurl,
+//     port:19701,
+//     username:"default",
+//     password: process.env.redispassword
 
 
-}
-const client=new Redis(configuration)
-app.use(express.json());
+// }
+// const client=new Redis(configuration)
+// app.use(express.json());
 
-module.exports={
-    client
-}
+// module.exports={
+//     client
+// }
